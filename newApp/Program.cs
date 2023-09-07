33a4ea849cc254276a0faf4e32033f61cbdf968a
dieuvip11 <dieuvip050102@gmail.com>
2023-09-07 17:20:58 +0700

    khai bao hang so voi cac kieu du lieu trong c#

--- newApp/Program.cs
+++ newApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, 2021050131!");
+﻿
+      
+    class Tranhoangdieuhangso
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tran Hoang Dieu");
+            Console.WriteLine("2021050131");            
+            //khai bao hang so pi
+            const double pi = 3.14159;            
+            double r;
+            Console.Write("Nhap ban kinh: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            double dien_tich = pi * r * r;
+            Console.WriteLine("\nBan kinh: {0}, Dien tich: {1}", r, dien_tich);
+            Console.ReadLine();            
+            Console.ReadKey();
+        }
+    }
+
+
