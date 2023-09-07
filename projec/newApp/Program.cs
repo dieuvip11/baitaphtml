ee491d56e4c64edf86dfce0b5909af592d85165d
dieuvip11 <dieuvip050102@gmail.com>
2023-09-07 21:16:20 +0700

    so sanh toann tu

--- projec/newApp/Program.cs
+++ projec/newApp/Program.cs
@@ -2,19 +2,18 @@
 
 Console.WriteLine("Tran Hoang Dieu");
 Console.WriteLine("2021050131");
-//phep chia lay nguyen va lay du 2 so
+// so sanh toan tu
 int a, b;
 Console.Write("gia tri cua a la: ");
 a = Convert.ToInt32(Console.ReadLine());
 Console.Write("gia tri cua b la: ");
 b = Convert.ToInt32(Console.ReadLine());
-int c = a - b;
-int d = a * b;
-int e = a / b;
-int f = a % b;
-Console.WriteLine("gia tri cua hieu = " + c);
-Console.WriteLine("gia tri cua tich = " + d);
-Console.WriteLine("gia tri cua thuong = " + e);
-Console.WriteLine("gia tri cua so du = " + f);
+ if (a>b){
+            Console.Write("b lon hon a ");
+        }else if(a<b){
+            Console.Write("b nho hon a");
+        }else{
+            Console.Write("b bang a ");
+        }
 
 
